03ea1dac83857dd93e6b9cdbd0c17c027ccb5ded
Matt Koppelman <mkoppelman@ebay.com>
2020-06-26 16:22:23 -0700

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -80,32 +80,11 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: _counter == 0
+            ? Text('You have built nothing...', style: Theme.of(context).textTheme.headline6)
+            : _counter == 1
+                ? Text('You have built 1 future!', style: Theme.of(context).textTheme.headline5)
+                : Text('You have built $_counter futures!!', style: Theme.of(context).textTheme.headline4),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
