e57a8b8297cd0c6674c0e5e4096053f19b9272ac
Larry McKenzie <lmckenzie@ebay.com>
2020-06-26 14:50:43 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FutureBuilders Hack20 Submission
+
+A Flutter application for the Flutter Community's Hack20 event.
---
+++ android/app/src/main/kotlin/com/hack20/futureBuilders/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hack20.futureBuilders
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
